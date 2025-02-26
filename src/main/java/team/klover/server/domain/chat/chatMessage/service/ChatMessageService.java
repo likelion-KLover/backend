4ac9ebd54bc316a0138fe44e70070612d03ca1b6
KOOTHEMAVERICK <121041290+KOOTHEMAVERICK@users.noreply.github.com>
2025-02-26 10:14:38 +0900

    Chat 도메인 기초 작업 완료

---
+++ src/main/java/team/klover/server/domain/chat/chatMessage/service/ChatMessageService.java
@@ -0,0 +1,4 @@
+package team.klover.server.domain.chat.chatMessage.service;
+
+public class ChatMessageService {
+}
---
+++ src/main/java/team/klover/server/domain/chat/chatRoom/serviceImpl/ChatRoomServiceImpl.java
@@ -0,0 +1,4 @@
+package team.klover.server.domain.chat.chatRoom.serviceImpl;
+
+public class ChatRoomServiceImpl {
+}
