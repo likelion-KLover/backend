22492166970bc92ca3e151f5d6af231ec00bbaa6
suuny0321 <joosun7647@naver.com>
2025-02-26 15:59:38 +0900

    swagger 연결

--- src/main/java/team/klover/server/domain/community/commPost/controller/ApiV1CommPostController.java
+++ src/main/java/team/klover/server/domain/community/commPost/controller/ApiV1CommPostController.java
@@ -1,5 +1,7 @@
 package team.klover.server.domain.community.commPost.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -17,8 +19,11 @@ import team.klover.server.global.common.response.KloverPage;
 import team.klover.server.global.exception.ReturnCode;
 import team.klover.server.global.util.AuthUtil;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RestController
-@RequestMapping("/api/v1/comm-post")
+@RequestMapping(value="/api/v1/comm-post",produces = APPLICATION_JSON_VALUE)
+@Tag(name="ApiV1CommPostController",description = "CommPost API")
 @RequiredArgsConstructor
 public class ApiV1CommPostController {
     private final CommPostService commPostService;
@@ -26,6 +31,7 @@ public class ApiV1CommPostController {
     // 사용자 위치 주변 게시글(관광지&사용자) 조회
     // http://localhost:8080/api/v1/comm-post/surroundings
     @GetMapping("/surroundings")
+    @Operation(summary = "사용자 위치 주변 게시글(관광지&사용자) 조회")
     public CombinedPostResponse getSurroundings(@ModelAttribute CommPostPage request, @RequestBody @Valid XYForm xyForm) {
         Pageable pageable = PageRequest.of(request.getPage(), request.getSize());
         return commPostService.findPostsWithinRadius(xyForm, pageable);
@@ -34,6 +40,7 @@ public class ApiV1CommPostController {
     // 본인 게시글 조회
     // http://localhost:8080/api/v1/comm-post/me
     @GetMapping("/me")
+    @Operation(summary = "본인 게시글 조회")
     public ApiResponse<CommPostDto> getMyCommPost(@ModelAttribute CommPostPage request) {
         Pageable pageable = PageRequest.of(request.getPage(), request.getSize());
         Long currentMemberId = AuthUtil.getCurrentMemberId();
@@ -43,6 +50,7 @@ public class ApiV1CommPostController {
     // 해당 게시글 상세 조회
     // http://localhost:8080/api/v1/comm-post/detail/1
     @GetMapping("/detail/{id}")
+    @Operation(summary = "해당 게시글 상세 조회")
     public ApiResponse<DetailCommPostDto> getDetailCommPost(@PathVariable("id") Long id) {
         return ApiResponse.of(commPostService.findById(id));
     }
@@ -50,6 +58,7 @@ public class ApiV1CommPostController {
     // 사용자가 저장한 게시글 조회
     // http://localhost:8080/api/v1/comm-post/collection
     @GetMapping("/collection")
+    @Operation(summary = "사용자가 저장한 게시글 조회")
     public ApiResponse<CommPostDto> getCollectionCommPost(@ModelAttribute CommPostPage request) {
         Pageable pageable = PageRequest.of(request.getPage(), request.getSize());
         Long currentMemberId = AuthUtil.getCurrentMemberId();
@@ -59,6 +68,7 @@ public class ApiV1CommPostController {
     // 사용자 닉네임 & 게시글 내용 검색
     // http://localhost:8080/api/v1/comm-post?keyword=게시글
     @GetMapping
+    @Operation(summary = "사용자 닉네임 & 게시글 내용 검색")
     public ApiResponse<CommPostDto> searchCommPost(@ModelAttribute CommPostPage request, @RequestParam("keyword") String keyword) {
         Pageable pageable = PageRequest.of(request.getPage(), request.getSize());
         return ApiResponse.of(KloverPage.of(commPostService.searchByNicknameAndContent(keyword, pageable)));
@@ -67,6 +77,7 @@ public class ApiV1CommPostController {
     // 해당 게시글 저장
     // http://localhost:8080/api/v1/comm-post/collection/1
     @PostMapping("/collection/{id}")
+    @Operation(summary = "해당 게시글 저장")
     public ApiResponse<String> addCollectionCommPost(@PathVariable("id") Long id){
         Long currentMemberId = AuthUtil.getCurrentMemberId();
         commPostService.addCollectionCommPost(currentMemberId, id);
@@ -76,6 +87,7 @@ public class ApiV1CommPostController {
     // 해당 게시글 저장 취소
     // http://localhost:8080/api/v1/comm-post/collection/1
     @DeleteMapping("/collection/{id}")
+    @Operation(summary = "해당 게시글 저장 취소")
     public ApiResponse<String> deleteCollectionCommPost(@PathVariable("id") Long id){
         Long currentMemberId = AuthUtil.getCurrentMemberId();
         commPostService.deleteCollectionCommPost(currentMemberId, id);
@@ -85,6 +97,7 @@ public class ApiV1CommPostController {
     // 게시글 좋아요
     // http://localhost:8080/api/v1/comm-post/like/1
     @PostMapping("/like/{id}")
+    @Operation(summary = "게시글 좋아요")
     public ApiResponse<String> addCommPostLike(@PathVariable("id") Long id){
         Long currentMemberId = AuthUtil.getCurrentMemberId();
         commPostService.addCommPostLike(currentMemberId, id);
@@ -94,6 +107,7 @@ public class ApiV1CommPostController {
     // 게시글 좋아요 취소
     // http://localhost:8080/api/v1/comm-post/like/1
     @DeleteMapping("/like/{id}")
+    @Operation(summary = "게시글 좋아요 취소")
     public ApiResponse<String> deleteCommPostLike(@PathVariable("id") Long id){
         Long currentMemberId = AuthUtil.getCurrentMemberId();
         commPostService.deleteCommPostLike(currentMemberId, id);
@@ -103,6 +117,7 @@ public class ApiV1CommPostController {
     // 게시글 생성
     // http://localhost:8080/api/v1/comm-post
     @PostMapping
+    @Operation(summary = "게시글 생성")
     public ApiResponse<String> addCommPost(@RequestBody @Valid CommPostForm commPostForm) {
         Long currentMemberId = AuthUtil.getCurrentMemberId();
         commPostService.addCommPost(currentMemberId, commPostForm);
@@ -112,6 +127,7 @@ public class ApiV1CommPostController {
     // 해당 게시글 수정
     // http://localhost:8080/api/v1/comm-post/1
     @PutMapping("/{id}")
+    @Operation(summary = "게시글 수정")
     public ApiResponse<String> updateCommPost(@PathVariable("id") Long id, @RequestBody @Valid CommPostForm commPostForm) {
         Long currentMemberId = AuthUtil.getCurrentMemberId();
         commPostService.updateCommPost(currentMemberId, id, commPostForm);
@@ -121,6 +137,7 @@ public class ApiV1CommPostController {
     // 해당 게시글 삭제
     // http://localhost:8080/api/v1/comm-post/1
     @DeleteMapping("/{id}")
+    @Operation(summary = "게시글 삭제")
     public ApiResponse<String> deleteCommPost(@PathVariable("id") Long id) {
         Long currentMemberId = AuthUtil.getCurrentMemberId();
         commPostService.deleteCommPost(currentMemberId, id);
