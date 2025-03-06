f822df6efd552124a329d801774053837d9bb78b
AsthuX <osm0348@naver.com>
2025-03-06 16:12:18 +0900

    fix: deepL api too many request 에러로 인한 다국어 처리 message.properties 로의 변경

--- src/main/java/team/klover/server/global/fcm/service/MessageBuildService.java
+++ src/main/java/team/klover/server/global/fcm/service/MessageBuildService.java
@@ -1,63 +1,54 @@
 package team.klover.server.global.fcm.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import team.klover.server.domain.notification.entity.NotificationMessage;
 import team.klover.server.domain.notification.enums.CustomFieldKey;
-import team.klover.server.global.translation.service.TranslationService;
+import team.klover.server.global.i18n.service.LocaleMessageService;
+
+import java.util.Locale;
 
 @Service
 @RequiredArgsConstructor
 public class MessageBuildService {
 
-    private final TranslationService translationService;
-    private final String titleTemplate = "You have a new notification!";
-
-    public NotificationMessage buildCommPostLikeMessage(NotificationMessage message, String userLanguage) throws JsonProcessingException {
+    public NotificationMessage buildCommPostLikeMessage(NotificationMessage message, String userLanguage) {
         String receiverNickname = (String) message.getCustomField().get(CustomFieldKey.RECEIVER_NICKNAME.getKeyName());
         String actorNickname = (String) message.getCustomField().get(CustomFieldKey.ACTOR_NICKNAME.getKeyName());
         String content = (String) message.getCustomField().get(CustomFieldKey.CONTENT.getKeyName());
 
-        String bodyTemplate = "{receiverNickname}'s post {content} was liked by {actorNickname}.";
-        String title = translationService.translateText(titleTemplate, userLanguage);
-        String body = translationService.translateTextPreservingVariables(bodyTemplate, userLanguage);
-
-        body = body.replace("{receiverNickname}", receiverNickname)
-                .replace("{actorNickname}", actorNickname)
-                .replace("{content}", content);
-        return message.toBuilder().title(title).body(body).build();
-    }
-
-    public NotificationMessage buildCommentCreateMessage(NotificationMessage message, String userLanguage) throws JsonProcessingException {
-        String receiverNickname = (String) message.getCustomField().get(CustomFieldKey.RECEIVER_NICKNAME.getKeyName());
-        String actorNickname = (String) message.getCustomField().get(CustomFieldKey.ACTOR_NICKNAME.getKeyName());
-        String content = (String) message.getCustomField().get(CustomFieldKey.CONTENT.getKeyName());
-
-        String title = translationService.translateText(titleTemplate, userLanguage);
-        String bodyTemplate = "{receiverNickname}'s post {content} was commented on by {actorNickname}.";
-        String body = translationService.translateTextPreservingVariables(bodyTemplate, userLanguage);
-
-        body = body.replace("{receiverNickname}", receiverNickname)
-                .replace("{actorNickname}", actorNickname)
-                .replace("{content}", content);
+        Locale locale = new Locale(userLanguage);
+        String title = getTitleByLocale(locale);
+        String body = LocaleMessageService.getMessage("notification.commPostLike", new Object[]{receiverNickname, content, actorNickname}, locale);
 
         return message.toBuilder().title(title).body(body).build();
     }
 
-    public NotificationMessage buildCommentLikeMessage(NotificationMessage message, String userLanguage) throws JsonProcessingException {
+    public NotificationMessage buildCommentCreateMessage(NotificationMessage message, String userLanguage) {
         String receiverNickname = (String) message.getCustomField().get(CustomFieldKey.RECEIVER_NICKNAME.getKeyName());
         String actorNickname = (String) message.getCustomField().get(CustomFieldKey.ACTOR_NICKNAME.getKeyName());
         String content = (String) message.getCustomField().get(CustomFieldKey.CONTENT.getKeyName());
 
-        String title = translationService.translateText(titleTemplate, userLanguage);
-        String bodyTemplate = "{receiverNickname}'s comment {content} was liked by {actorNickname}.";
-        String body = translationService.translateTextPreservingVariables(bodyTemplate, userLanguage);
-
-        body = body.replace("{receiverNickname}", receiverNickname)
-                .replace("{actorNickname}", actorNickname)
-                .replace("{content}", content);
+        Locale locale = new Locale(userLanguage);
+        String title = getTitleByLocale(locale);
+        String body = LocaleMessageService.getMessage("notification.commentCreate", new Object[]{receiverNickname, content, actorNickname}, locale);
 
         return message.toBuilder().title(title).body(body).build();
+    }
+
+    public NotificationMessage buildCommentLikeMessage(NotificationMessage message, String userLanguage) {
+        String receiverNickname = (String) message.getCustomField().get(CustomFieldKey.RECEIVER_NICKNAME.getKeyName());
+        String actorNickname = (String) message.getCustomField().get(CustomFieldKey.ACTOR_NICKNAME.getKeyName());
+        String content = (String) message.getCustomField().get(CustomFieldKey.CONTENT.getKeyName());
+
+        Locale locale = new Locale(userLanguage);
+        String title = getTitleByLocale(locale);
+        String body = LocaleMessageService.getMessage("notification.commentLike", new Object[]{receiverNickname, content, actorNickname}, locale);
+
+        return message.toBuilder().title(title).body(body).build();
+    }
+
+    private String getTitleByLocale(Locale locale) {
+        return LocaleMessageService.getMessage("notification.title", null, locale);
     }
 }
--- src/main/java/team/klover/server/global/i18n/service/LocaleMessageService.java
+++ src/main/java/team/klover/server/global/i18n/service/LocaleMessageService.java
@@ -2,10 +2,15 @@ package team.klover.server.global.i18n.service;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+
 
 @Service
 public class LocaleMessageService {
+
     private static MessageSource messageSource;
 
     public LocaleMessageService(MessageSource messageSource) {
@@ -14,5 +19,9 @@ public class LocaleMessageService {
 
     public static String getMessage(String code) {
         return messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
+    }
+
+    public static String getMessage(String code, @Nullable Object[] args, Locale locale) {
+        return messageSource.getMessage(code, args, locale);
     }
 }
