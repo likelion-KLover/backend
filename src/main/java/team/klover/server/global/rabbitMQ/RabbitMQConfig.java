b89bc0b680b563332b30a3c33e199d885988fbc3
AsthuX <osm0348@naver.com>
2025-03-06 16:59:41 +0900

    fix: retry policy 구현

--- src/main/java/team/klover/server/global/rabbitMQ/RabbitMQConfig.java
+++ src/main/java/team/klover/server/global/rabbitMQ/RabbitMQConfig.java
@@ -1,9 +1,15 @@
 package team.klover.server.global.rabbitMQ;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.retry.RejectAndDontRequeueRecoverer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import team.klover.server.global.rabbitMQ.queueNames.QueueNames;
+
+import java.time.Duration;
 
 @Configuration
 public class RabbitMQConfig {
@@ -33,4 +39,19 @@ public class RabbitMQConfig {
     @Bean
     public Queue esTourPostUpdateQueue() { return new Queue(QueueNames.ES_TOURPOST_UPDATE.name(), true);}
 
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(CachingConnectionFactory cachingConnectionFactory) {
+        var containerFactory = new SimpleRabbitListenerContainerFactory();
+        containerFactory.setConnectionFactory(cachingConnectionFactory);
+
+        containerFactory.setAdviceChain(
+                RetryInterceptorBuilder.stateless()
+                        .maxAttempts(3)
+                        .backOffOptions(Duration.ofSeconds(3L).toMillis(), 2, Duration.ofSeconds(10L).toMillis())
+                        .recoverer(new RejectAndDontRequeueRecoverer())
+                        .build()
+        );
+
+        return containerFactory;
+    }
 }
