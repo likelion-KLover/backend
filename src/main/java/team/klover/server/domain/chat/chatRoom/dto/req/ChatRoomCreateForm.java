ce1caf71de5c4e6f91ee075ce229515a469d187d
LSH-kw0315 <tmdgjsdl1234@naver.com>
2025-03-13 14:01:42 +0900

    test: attempt to use chatting in front-end

--- src/main/java/team/klover/server/domain/chat/chatRoom/dto/req/ChatRoomCreateForm.java
+++ src/main/java/team/klover/server/domain/chat/chatRoom/dto/req/ChatRoomCreateForm.java
@@ -15,8 +15,8 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL) // null 값은 직렬화에서 제외
-public class ChatRoomForm {
+public class ChatRoomCreateForm {
     private String title;
     @Builder.Default
-    private List<ChatRoomMember> chatRoomMembers = new ArrayList<>();
+    private List<Long> memberIds = new ArrayList<>();
 }
